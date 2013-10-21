f04131c4aa89b8634a977a1b7d15c0c032820952
Greg Burghardt <greg_burghardt@yahoo.com>
2013-10-21 09:29:49 -0400

    Initial commit.

---
+++ src/template.js
@@ -0,0 +1,248 @@
+// <script type="text/html" data-template-name="blog/post_body">
+// 	#{include blog/header}
+//
+// 	<h1>#{title}</h1>
+// 	<p>#{body}</p>
+// 	<ol>
+// 		<li>#{render blog/post/comments with comments}</li>
+// 	</ol>
+//
+// 	#{include blog/footer}
+// </script>
+
+function Template(name, source) {
+	if (name) {
+		this.name = name;
+		this.setSource(source);
+	}
+}
+
+Template.prototype = {
+
+	name: null,
+
+	source: null,
+
+	subTemplatesFetched: false,
+
+	constructor: Template,
+
+	render: function render(data) {
+		var key, source = this.source, regexRender = Template.REGEX_RENDER, regexInclude = Template.REGEX_INCLUDE;
+
+		var renderReplacer = function(tag, templateName, withClause, dataKey) {
+			var renderData = (!dataKey) ? data : data[ dataKey ];
+			if (renderData instanceof Array) {
+				var buffer = [], i = 0, length = renderData.length, template = Template.find(templateName), str;
+
+				for (i; i < length; i++) {
+					buffer.push( template.render( renderData[i] ) );
+				}
+
+				str = buffer.join("");
+
+				template = buffer = null;
+
+				return str;
+			}
+			else {
+				return Template.find(templateName).render( renderData );
+			}
+		};
+
+		var includeReplacer = function(tag, templateName) {
+			return Template.find(templateName).render(data);
+		};
+
+		var doReplace = function(key) {
+			var regex = new RegExp("#\\{\\s*" + key.replace(/\./g, "\\\.", "g") + "\\s*\\}", "g");
+
+			// replace #{foo} tags with value at data[foo]
+			source = source.replace(regex, data[key] || "");
+
+			// replace #{render with foo} tags by rendering data[foo]
+			if (source.match(regexRender)) {
+				source = source.replace(regexRender, renderReplacer);
+			}
+
+			// replace #{include} tags
+			if (source.match(Template.templates[name])) {
+				source = source.replace(regexInclude, includeReplacer);
+			}
+		};
+
+		if (data.getTemplateKeys) {
+			var keys = data.getTemplateKeys(), i = 0, length = keys.length;
+
+			for (i; i < length; i++) {
+				doReplace(keys[i]);
+			}
+		}
+		else {
+			for (key in data) {
+				if (data.hasOwnProperty(key)) {
+					doReplace(key);
+				}
+			}
+		}
+
+		// clean up unmatched template tags
+		source = source.replace(/#\{.+?\}/g, "");
+
+		data = regexRender = regexInclude = renderReplacer = renderInclude = null;
+
+		return source;
+	},
+
+	setSource: function setSource(source) {
+		if (typeof source === "string") {
+			this.source = source;
+		}
+		else {
+			this.source = source.innerHTML;
+			this.name = source.getAttribute("data-template-name");
+		}
+
+		source = null;
+	}
+
+};
+
+Template.cacheBuster = new Date().getTime();
+
+Template.document = window.document;
+
+Template.REGEX_INCLUDE = /#\{\s*include\s+(.+?)\s*\}/g;
+
+Template.REGEX_RENDER = /#\{\s*render\s+(.+?)(\s+with\s+(.*?)\s*)?\}/g;
+
+Template.templates = {};
+
+Template.fetch = function fetch(name, context, callback) {
+	if (Template.templates[name]) {
+		callback.call(context, Template.templates[name]);
+	}
+	else {
+		var source = Template.getTemplateSourceNode(name);
+		var url = source.getAttribute("data-src");
+		var xhr;
+
+		var cleanup = function() {
+			context = callback = xhr = source = cleanup = null;
+		};
+
+		if (url) {
+			url = url + (/\?/.test(url) ? "&" : "?") + "cacheBuster=" + Template.cacheBuster;
+			xhr = new XMLHttpRequest();
+			xhr.open("GET", url);
+			xhr.onreadystatechange = function() {
+				var success = (this.status === 201 || this.status === 200) ? true : false;
+
+				if (this.readyState === 4 && success) {
+					if (success) {
+						Template.fetchSubTemplates(xhr.responseText, function() {
+							Template.templates[name] = new Template(name, xhr.responseText);
+							callback.call(context, Template.templates[name]);
+							cleanup();
+						});
+					}
+					else if (this.status === 403) {
+						cleanup();
+						throw new Error("Failed to fetch template from URL: " + url + ". Server returned 403 Not Authorized");
+					}
+					else if (this.status === 404) {
+						cleanup();
+						throw new Error("Failed to fetch template from URL: " + url + ". Server returned 404 Not Found.");
+					}
+					else if (this.status >= 400) {
+						cleanup();
+						throw new Error("Failed to fetch template from URL: " + url + ". Server returned an error (" + this.status + ")");
+					}
+				}
+			};
+			xhr.send(null);
+		}
+		else {
+			Template.templates[name] = new Template(name, source);
+			Template.fetchSubTemplates(source.innerHTML, function() {
+				callback.call(context, Template.templates[name]);
+				cleanup();
+			});
+		}
+	}
+};
+
+Template.fetchSubTemplates = function fetchSubTemplates(source, callback) {
+	var subTemplates = [], total, i = 0, count = 0;
+
+	var handleTemplateFetched = function() {
+		count++;
+
+		if (count === total) {
+			callback();
+		}
+	};
+
+	source.replace(this.REGEX_RENDER, function(tag, templateName, dataKey) {
+		subTemplates.push(templateName);
+	}).replace(this.REGEX_INCLUDE, function(tag, templateName) {
+		subTemplates.push(templateName);
+	});
+
+	total = subTemplates.length;
+
+	if (total) {
+		for (i = 0; i < total; i++) {
+			Template.fetch(subTemplates[i], this, handleTemplateFetched);
+		}
+	}
+	else {
+		callback();
+	}
+};
+
+Template.find = function find(name) {
+	if (!Template.templates[name]) {
+		var source = Template.getTemplateSourceNode(name);
+		Template.templates[name] = new Template(name, source);
+		source = null;
+	}
+
+	return Template.templates[name];
+};
+
+Template.getTemplateSourceNode = function getTemplateSourceNode(name) {
+	var source = Template.document.querySelector("script[data-template-name=" + name.replace(/\//g, "\\/") + "]");
+
+	if (!source) {
+		throw new Error('Missing template ' + name + '. Required: <script type="text/html" data-template-name="' + name + '"></script>');
+	}
+
+	return source;
+};
+
+Template.parseHTML = function parseHTML(html) {
+	var div = document.createElement("div")
+	var nodes = [], i, length, node;
+
+	div.innerHTML = html.replace(/^\s+|\s+$/g, "");
+	i = div.childNodes.length;
+
+	while (i--) {
+		node = div.childNodes[i];
+		node.parentNode.removeChild(node);
+		nodes.push(node);
+	}
+
+	node = div = null;
+
+	return nodes;
+};
+
+Template.render = function render(name, data, context, callback) {
+	Template.fetch(name, this, function(template) {
+		var source = template.render(data);
+		callback.call(context, source, template);
+		data = context = callback = template = null;
+	});
+};
