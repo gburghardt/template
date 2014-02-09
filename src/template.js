7d5b90cde78e499a0ba9956bda6d1ccf3e057bf7
Greg Burghardt <greg_burghardt@yahoo.com>
2014-02-09 16:46:50 -0500

    Removing view resolver code

--- src/template.js
+++ src/template.js
@@ -13,17 +13,32 @@
 function Template(name, source) {
 	if (name) {
 		this.name = name;
+	}
+
+	if (source) {
 		this.setSource(source);
 	}
 }
+
+Template.REGEX_RENDER = /#\{\s*render\s+(.+?)(\s+with\s+(.*?)\s*)?\}/g;
+Template.REGEX_INCLUDE = /#\{\s*include\s+(.+?)\s*\}/g;
+Template.REGEX_FOREACH = /#\{\s*render\s+(.+?)\s+foreach(\s+([^\s}]+))?\s*\}/g;
+
+Template.viewResolver = null;
+
+Template.find = function find(name) {
+	if (!this.viewResolver) {
+		throw new Error("No view resolver found.");
+	}
+
+	return this.viewResolver.find(name);
+};
 
 Template.prototype = {
 
 	name: null,
 
 	source: null,
-
-	subTemplatesFetched: false,
 
 	constructor: Template,
 
@@ -132,160 +147,4 @@ Template.prototype = {
 		source = null;
 	}
 
-};
-
-Template.REGEX_RENDER = /#\{\s*render\s+(.+?)(\s+with\s+(.*?)\s*)?\}/g;
-Template.REGEX_INCLUDE = /#\{\s*include\s+(.+?)\s*\}/g;
-Template.REGEX_FOREACH = /#\{\s*render\s+(.+?)\s+foreach(\s+([^\s}]+))?\s*\}/g;
-
-Template.cacheBuster = new Date().getTime();
-
-Template.document = window.document;
-
-Template.templates = {};
-
-Template.fetch = function fetch(name, context, callback) {
-	if (Template.find(name)) {
-		callback.call(context, Template.templates[name]);
-	}
-	else {
-		var source = Template.getTemplateSourceNode(name);
-		var url = source.getAttribute("data-src");
-		var xhr;
-
-		var cleanup = function() {
-			context = callback = xhr = source = cleanup = null;
-		};
-
-		if (url) {
-			url = url + (/\?/.test(url) ? "&" : "?") + "cacheBuster=" + Template.cacheBuster;
-			xhr = new XMLHttpRequest();
-			xhr.open("GET", url);
-			xhr.onreadystatechange = function() {
-				var success = (this.status === 201 || this.status === 200) ? true : false;
-
-				if (this.readyState === 4 && success) {
-					if (success) {
-						Template.fetchSubTemplates(xhr.responseText, function() {
-							Template.templates[name] = new Template(name, xhr.responseText);
-							callback.call(context, Template.templates[name]);
-							cleanup();
-						});
-					}
-					else if (this.status === 403) {
-						cleanup();
-						throw new Error("Failed to fetch template from URL: " + url + ". Server returned 403 Not Authorized");
-					}
-					else if (this.status === 404) {
-						cleanup();
-						throw new Error("Failed to fetch template from URL: " + url + ". Server returned 404 Not Found.");
-					}
-					else if (this.status >= 400) {
-						cleanup();
-						throw new Error("Failed to fetch template from URL: " + url + ". Server returned an error (" + this.status + ")");
-					}
-				}
-			};
-			xhr.send(null);
-		}
-		else {
-			Template.templates[name] = new Template(name, source);
-			Template.fetchSubTemplates(source.innerHTML, function() {
-				callback.call(context, Template.templates[name]);
-				cleanup();
-			});
-		}
-	}
-};
-
-Template.fetchSubTemplates = function fetchSubTemplates(source, callback) {
-	var subTemplates = [], total, i = 0, count = 0;
-
-	var handleTemplateFetched = function() {
-		count++;
-
-		if (count === total) {
-			callback();
-		}
-	};
-
-	source
-		.replace(this.REGEX_FOREACH, function(tag, templateName) {
-			subTemplates.push(templateName);
-		})
-		.replace(this.REGEX_RENDER, function(tag, templateName, dataKey) {
-			subTemplates.push(templateName);
-		})
-		.replace(this.REGEX_INCLUDE, function(tag, templateName) {
-			subTemplates.push(templateName);
-		});
-
-	total = subTemplates.length;
-
-	if (total) {
-		for (i = 0; i < total; i++) {
-			Template.fetch(subTemplates[i], this, handleTemplateFetched);
-		}
-	}
-	else {
-		callback();
-	}
-};
-
-Template.find = function find(name) {
-	if (!Template.templates[name]) {
-		var source = Template.getTemplateSourceNode(name);
-		Template.templates[name] = new Template(name, source);
-		source = null;
-	}
-
-	return Template.templates[name];
-};
-
-Template.getTemplateSourceNode = function getTemplateSourceNode(name) {
-	var scripts = Template.document.getElementsByTagName("script"),
-	    i = 0, source;
-
-	for (i; i < scripts.length; i++) {
-		if (scripts[i].getAttribute("data-template-name") === name) {
-			source = scripts[i].innerHTML;
-			break;
-		}
-	}
-
-	if (!source) {
-		throw new Error('Missing template ' + name + '. Required: <script type="text/html" data-template-name="' + name + '"></script>');
-	}
-
-	return source;
-};
-
-Template.parseHTML = function parseHTML(html) {
-	var div = document.createElement("div")
-	var nodes = [], i, length, node;
-
-	div.innerHTML = html.replace(/^\s+|\s+$/g, "");
-	i = div.childNodes.length;
-
-	while (i--) {
-		node = div.childNodes[i];
-		node.parentNode.removeChild(node);
-		nodes.push(node);
-	}
-
-	node = div = null;
-
-	return nodes;
-};
-
-Template.register = function register(template) {
-	Template.templates[ template.name ] = template;
-};
-
-Template.render = function render(name, data, context, callback) {
-	Template.fetch(name, this, function(template) {
-		var source = template.render(data);
-		callback.call(context, source, template);
-		data = context = callback = template = null;
-	});
 };
